f141b647f1fb6a86421b33bdd7ca727379502ba3
blonded <1021680569@qq.com>
2021-05-15 14:43:47 +0800

    更新注释

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -11,7 +11,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	char s[2];//接收-c or -w
 	int i;//读取当前字符
 	int count1 = 0;//统计字符数
-	int count2 = 1;//统计单词数
+	int count2 = 1;//统计单词数，文本不为空则必然有一个单词所以设初值为1
 	fl = fopen(c, "r");
 	scanf("%s", s);
 	if (!strcmp(s, "-c")){
@@ -21,7 +21,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		goto case2;
 	}
 case1:
-	while ((i = fgetc(fl)) != EOF)
+	while ((i = fgetc(fl)) != EOF)//通过逐个读取字符数的方法统计所有字符的数量
 		count1++;
 	printf("字符数：%d", count1);
 	fclose(fl);
@@ -30,13 +30,13 @@ case2:
 	char s1[1024]="";
 	fgets(s1, 1000, fl);
 	for (int a = 0; a<strlen(s1); a++){
-		if (s1[a] == ' ' || s1[a] == ',' || s1[a] == '\t'){
-			if (s1[a + 1] != ' ' && s1[a + 1] != ',' && s1[a + 1] != '\t')
-				count2++;
+		if (s1[a] == ' ' || s1[a] == ',' || s1[a] == '\t'){//如果读取到空格，逗号或制表符则开始判断
+			if (s1[a + 1] != ' ' && s1[a + 1] != ',' && s1[a + 1] != '\t')//如果不是空格，逗号或制表符相连，则空格分开的是两个单词
+				count2++;//单词数就加1
 		}
 	}
-	if (s1[strlen(s1)] == ' ' || s1[strlen(s1)] == ',' || s1[strlen(s1)] == '\t')
-		count2--;
+	if (s1[strlen(s1)] == ' ' || s1[strlen(s1)] == ',' || s1[strlen(s1)] == '\t')//如果文本的最后是空格，逗号或制表符，则计算的单词数会比实际的多1
+		count2--;//因此需要减掉1
 	printf("单词数：%d\n", count2);
 	fclose(fl);
 	return 0;
